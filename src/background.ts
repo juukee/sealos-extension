8c49bae087393c38bc1ba093e84958cb8e53e4fc
卖男孩的小火柴 <913648653@qq.com>
2023-06-15 23:31:10 +0800

    first commit

---
+++ src/background.ts
@@ -0,0 +1,27 @@
+import { browser } from "webextension-polyfill-ts";
+import { ConfigProvider } from "./config";
+
+async function gitpodifyCurrentTab() {
+    try {
+        // add a dummy div element to indicate that gitpodify.bundle.js was injected by a user click on the gitpod icon
+        browser.tabs.executeScript({ code: "document.body.innerHTML += '<div style=\"display: none;\" id=\"gitpod-extension-icon-clicked\"></div>'" })
+        browser.tabs.executeScript({ file: "/dist/bundles/gitpodify.bundle.js" });
+    } catch {
+        try {
+            const configProvider = await ConfigProvider.create();
+            const config = configProvider.getConfig();
+            window.open(config.gitpodURL);
+        } catch {
+            window.open("https://cloud.sealos.io/");
+        }
+    }
+}
+
+browser.browserAction.onClicked.addListener(gitpodifyCurrentTab)
+
+browser.runtime.onInstalled.addListener((details) => {
+    if (details.reason === "install") {
+        window.open("https://www.cloud.sealos.io/extension-activation?track=true");
+    }
+});
+browser.runtime.setUninstallURL("https://www.cloud.sealos.io/extension-uninstall?track=true");
---
+++ src/injectors/github-injector.ts
@@ -0,0 +1,245 @@
+import * as select from 'select-dom';
+import * as ghInjection from 'github-injection';
+import { ConfigProvider } from '../config';
+import { ButtonInjector, InjectorBase, checkIsBtnUpToDate, rewritePeriodKeybindGitHub } from './injector';
+import { renderSealosUrl, makeOpenInPopup } from '../utils';
+
+namespace Sealosify {
+	export const NAV_BTN_ID = "gitpod-btn-nav";
+	export const NAV_BTN_CLASS = "gitpod-nav-btn";
+    export const NAV_BTN_CLASS_SELECTOR = "." + NAV_BTN_CLASS;
+
+    export const EDIT_BTN_ID = "gitpod-btn-file";
+
+    export const CSS_REF_BTN_CONTAINER = "gitpod-btn-container";
+    export const CSS_REF_NO_CONTAINER = "no-container";
+}
+
+/**
+ * This implementation currently assumes that there is only ever one button per page
+ */
+export class GitHubInjector extends InjectorBase {
+
+    constructor(configProvider: ConfigProvider) {
+        super(configProvider, [
+            new PullInjector(),
+            new IssueInjector(),
+            new FileInjector(),
+            new EditFileButtonInjector(),
+            new NavigationInjector(),
+            new EmptyRepositoryInjector(),
+        ]);
+    }
+
+    canHandleCurrentPage(): boolean {
+        // TODO Does this work for GitHub Enterprise, too?
+        const metaTags = document.getElementsByTagName("meta");
+        for (let i = 0; i < metaTags.length; i++) {
+            const metaTag = metaTags[i];
+            if (metaTag.name === "hostname" && metaTag.content.includes("github")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    checkIsInjected(): boolean {
+        const button = document.getElementById(`${Sealosify.NAV_BTN_ID}`);
+        const currentUrl = renderSealosUrl(this.config.gitpodURL);
+        return checkIsBtnUpToDate(button, currentUrl);
+    }
+
+    async inject(): Promise<void> {
+        // ghInjection triggers an event whenever only parts of the GitHub page have been reloaded
+	    ghInjection(() => {
+            if (!this.checkIsInjected()) {
+                this.injectButtons();
+            }
+            
+            (async () => {
+               await rewritePeriodKeybindGitHub();
+            })();
+        });
+    }
+
+    async update(): Promise<void> {
+        this.injectButtons();
+    }
+}
+
+abstract class ButtonInjectorBase implements ButtonInjector {
+
+    constructor(
+        protected readonly parentSelector: string,
+        protected readonly btnClasses: string,
+        protected readonly float: boolean = true,
+        protected readonly asFirstChild: boolean = false
+    ) {}
+
+    abstract isApplicableToCurrentPage(): boolean;
+
+    inject(currentUrl: string, openAsPopup: boolean) {
+        const actionbar = select(this.parentSelector);
+        if (!actionbar) {
+            return;
+        }
+
+        const oldBtn = document.getElementById(Sealosify.NAV_BTN_ID);
+        if (oldBtn) {
+            if (!checkIsBtnUpToDate(oldBtn, currentUrl)) {
+                // update button
+                (oldBtn as HTMLAnchorElement).href = currentUrl;
+            }
+            // button is there and up-to-date
+            return;
+        }
+
+        const btn = this.renderButton(currentUrl, openAsPopup);
+
+        const btnGroup = actionbar.getElementsByClassName("BtnGroup");
+        const detailsBtn = Array.from(actionbar.children)
+            .filter(child => child.tagName.toLowerCase() === "details" && child.id.endsWith("more-options-details"));
+        if (btnGroup && btnGroup.length > 0 && btnGroup[0].classList.contains('float-right')) {
+            actionbar.insertBefore(btn, btnGroup[0]);
+        } else if (detailsBtn && detailsBtn.length > 0) {
+            if (detailsBtn[0].previousElementSibling) {
+                detailsBtn[0].previousElementSibling.classList.remove("mr-2","ml-2");
+            }
+            btn.classList.add("mr-2","ml-2");
+            actionbar.insertBefore(btn, detailsBtn[0]);
+        } else if (this.asFirstChild && actionbar) {
+            actionbar.insertBefore(btn, actionbar.firstChild);
+        } else {
+            actionbar.appendChild(btn);
+        }
+
+        const primaryButtons = actionbar.getElementsByClassName("btn-primary");
+        if (primaryButtons && primaryButtons.length > 1) {
+            Array.from(primaryButtons)
+            .slice(0, primaryButtons.length - 1)
+            .forEach(primaryButton => primaryButton.classList.replace("btn-primary", "btn-secondary"));
+        }
+
+        // Edit File Menu Options - Open in Sealos
+        const editFileButton = document.querySelector('.Box-header .select-menu .SelectMenu-list') as ParentNode;
+        if (editFileButton) {
+            editFileButton.prepend(this.renderEditButton(currentUrl, openAsPopup));
+        }
+    }
+
+    protected renderButton(url: string, openAsPopup: boolean): HTMLElement {
+        let classes = this.btnClasses + ` ${Sealosify.NAV_BTN_CLASS}`;
+        if (this.float) {
+            classes = classes + ` float-right pr-1`;
+        }
+
+        const container = document.createElement('div');
+        container.id = Sealosify.CSS_REF_BTN_CONTAINER;
+        container.className = classes;
+
+        const a = document.createElement('a');
+        a.id = Sealosify.NAV_BTN_ID;
+        a.title = "Sealos";
+        a.text = "Sealos"
+        a.href = url;
+        a.target = "_blank";
+        if (openAsPopup) {
+            makeOpenInPopup(a);
+        }
+        a.className = "btn btn-sm btn-primary";
+
+        this.adjustButton(a);
+
+        container.appendChild(a);
+        return container;
+    }
+    protected adjustButton(a: HTMLAnchorElement) {
+        // do nothing
+    }
+
+    protected renderEditButton(url: string, openAsPopup: boolean): HTMLElement {
+        const a = document.createElement('a');
+        a.id = Sealosify.EDIT_BTN_ID;
+        a.title = "Edit this file in Sealos";
+        a.text = "Open in Sealos";
+        a.href = url;
+        a.target = "_blank";
+        if (openAsPopup) {
+            makeOpenInPopup(a);
+        }
+        a.className = "SelectMenu-item js-blob-dropdown-click width-full d-flex flex-justify-between color-fg-default f5 text-normal";
+        return a;
+    }
+}
+
+class PullInjector extends ButtonInjectorBase {
+    constructor() {
+        super(".gh-header-actions", "");
+    }
+
+    isApplicableToCurrentPage(): boolean {
+		return window.location.pathname.includes("/pull/");
+    }
+}
+
+class IssueInjector extends ButtonInjectorBase {
+    constructor() {
+        super(".gh-header-actions", "");
+    }
+
+    isApplicableToCurrentPage(): boolean {
+		return window.location.pathname.includes("/issues/");
+    }
+}
+
+class EditFileButtonInjector extends ButtonInjectorBase {
+    constructor() {
+        super("", "gitpod-file-edit-btn");
+    }
+
+    isApplicableToCurrentPage(): boolean {
+        return window.location.pathname.includes("/blob/");
+    }
+}
+
+class FileInjector extends ButtonInjectorBase {
+    constructor() {
+        super(".repository-content > div > div > div", "gitpod-file-btn");
+    }
+
+    protected adjustButton(a: HTMLAnchorElement): void {
+        a.className = "btn btn-primary";
+    }
+
+    isApplicableToCurrentPage(): boolean {
+        return window.location.pathname.includes("/blob/");
+    }
+}
+
+class NavigationInjector extends ButtonInjectorBase {
+    constructor() {
+        super(".file-navigation", "empty-icon position-relative");
+    }
+
+    protected adjustButton(a: HTMLAnchorElement): void {
+        a.className = "btn btn-primary";
+    }
+
+    isApplicableToCurrentPage(): boolean {
+        return !!select.exists(".file-navigation");
+    }
+}
+
+class EmptyRepositoryInjector extends ButtonInjectorBase {
+    constructor() {
+        super(".repository-content > div > git-clone-help > div > div ", "gitpod-file-btn", false, true);
+    }
+
+    protected adjustButton(a: HTMLAnchorElement): void {
+        a.className = "btn btn-sm btn-primary";
+    }
+
+    isApplicableToCurrentPage(): boolean {
+        return !!select.exists("#empty-setup-clone-url");
+    }
+}
